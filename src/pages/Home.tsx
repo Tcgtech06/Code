413799b77dbfb260693ac2762b09efc6bb8ee669
Your Name <your_email@example.com>
2026-04-30 07:30:25 +0530

    Fix spelling: Dedication in Knitinfo review

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -189,7 +189,7 @@ const Home: React.FC = () => {
       name: "KNITINFO",
       logo: "/Images/knitinfo.png",
       originalLogo: "/Images/knitinfo.png",
-      review: "TCG TECH technical Team is very Good with full of Young Blood's and Their Deddication is Huge , They Build me an Hybrid Web App that is Awesome",
+      review: "TCG TECH technical Team is very Good with full of Young Blood's and Their Dedication is Huge , They Build me an Hybrid Web App that is Awesome",
       rating: 5,
       author: "MD, KNITINFO",
       website: "https://knitinfo.in"
